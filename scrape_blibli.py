91860ec0253abd5ef8637705918806069384ab7d
meldawonu <trimeldawanim@gmail.com>
2026-08-19 13:10:40 +0700

    add: arguments

--- scrape_blibli.py
+++ scrape_blibli.py
@@ -23,6 +23,8 @@ options = uc.ChromeOptions()
 options.add_argument("--headless=new") # sementara ini bisa digunakan jika ingin menjalankan browser tanpa GUI
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 # Opsional: jalankan tanpa GUI/headless jika dibutuhkan
 # options.add_argument("--headless")
 
